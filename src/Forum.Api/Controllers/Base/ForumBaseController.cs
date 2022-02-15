0d3e74ab3b7f4a6777e51c231b69d4ed9231081d
Trevor Pierce <tpierce42@ivytech.edu>
2022-02-15 15:01:29 -0500

    Added base controller to inherit from in Forum.Api

---
+++ src/Forum.Api/Controllers/Base/ForumBaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Api.Controllers.Base;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public abstract class ForumBaseController : ControllerBase { }
