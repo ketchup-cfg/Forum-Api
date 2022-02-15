2160c63050c01f95bf9b770e16576cd12a336694
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-02-15 14:50:59 -0500

    Finally got unit tests to work...

--- test/Data.Tests/Fixtures/DatabaseFixture.cs
+++ test/Data.Tests/Fixtures/DatabaseFixture.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
 using Forum.Data;
 using Microsoft.Extensions.Configuration;
 
