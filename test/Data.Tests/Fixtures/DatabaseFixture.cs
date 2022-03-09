401b26099b93a4c55423e3fa697cf85263c756c5
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-03-08 20:24:23 -0500

    Update to test/Data.Tests DatabaseFixture to use a new TestDatabase class that implements IDatabase and connects to a test database using an environment variable instead of pulling from a appsettings.Development.json file

--- test/Data.Tests/Fixtures/DatabaseFixture.cs
+++ test/Data.Tests/Fixtures/DatabaseFixture.cs
@@ -1,23 +1,14 @@
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
-using System.Reflection.PortableExecutable;
-using Forum.Data;
-using Microsoft.Extensions.Configuration;
 
 namespace Data.Tests.Fixtures;
 
 public class DatabaseFixture : IDisposable
 {
-    public Database Database { get; }
+    public TestDatabase Database { get; }
     
     public DatabaseFixture()
     {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.Development.json")
-            .Build();
-        Database = new Database(config);
+        Database = new TestDatabase();
     }
     
     public void Dispose()
