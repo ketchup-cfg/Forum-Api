abb3d9c7baf432ab07e79511e0ecdbf55aa5ec60
Trevor Pierce <30775195+ketchup-cfg@users.noreply.github.com>
2022-03-10 13:59:37 -0500

    Added POST functionality for /api/topics endpoint, and refactored unit test libraries a bit (#11)

--- src/Forum.Api/Controllers/TopicsController.cs
+++ src/Forum.Api/Controllers/TopicsController.cs
@@ -19,8 +19,6 @@ public class TopicsController : ForumBaseController
     /// Find and return a collection of all existing forum topics.
     /// </summary>
     /// <returns>A collection containing data for all existing forum topics, if any exist.</returns>
-    /// <response code="200">Topics are defined and were able to be located.</response>
-    /// <response code="204">No topics are currently defined.</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType((StatusCodes.Status204NoContent))]
@@ -43,8 +41,6 @@ public class TopicsController : ForumBaseController
     /// </summary>
     /// <param name="id">The unique ID for a single topic.</param>
     /// <returns>The data for the forum topic that was found using the provided ID.</returns>
-    /// <response code="200">The requested topic was found.</response>
-    /// <response code="404">No topic was found matching the ID passed into the URI path.</response>
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -62,18 +58,38 @@ public class TopicsController : ForumBaseController
     /// </summary>
     /// <param name="name">The unique name for a single topic.</param>
     /// <returns>The data for the forum topic that was found using the provided name.</returns>
-    /// <response code="200">The requested topic was found.</response>
-    /// <response code="404">No topic was found matching the name passed into the URI path.</response>
     [HttpGet("{name}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<Topic>> GetTopicByName([FromRoute] string name)
+    public async Task<IActionResult> GetTopicByName([FromRoute] string name)
     {
         var topic = await _topics.GetTopicByName(name);
 
         if (topic is null) return NotFound();
 
-        return topic;
+        return Ok(topic);
+    }
+
+    /// <summary>
+    /// Create a new topic.
+    /// </summary>
+    /// <param name="topic">The new topic to be created.</param>
+    /// <returns>The newly created topic, including its updated and current ID.</returns>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateTopic([FromBody] Topic topic)
+    {
+        try
+        {
+            topic.Id = await _topics.CreateTopic(topic);
+        }
+        catch (PostgresException e) when (e.SqlState == PostgresErrorCodes.UniqueViolation)
+        {
+            return BadRequest("Topic name is not unique and is already defined for another topic");
+        }
+
+        return CreatedAtAction(nameof(GetTopicById), new {id = topic.Id}, topic);
     }
 
     /// <summary>
@@ -81,27 +97,13 @@ public class TopicsController : ForumBaseController
     /// </summary>
     /// <param name="id">The unique ID for a single topic.</param>
     /// <param name="topic">The topic values to replace the existing topic's values with.</param>
-    /// <remarks>
-    /// Sample request:
-    ///
-    ///     PUT /api/topics/1
-    ///     {
-    ///        "id": 1,
-    ///        "name": "Weather",
-    ///        "Description": "Some weather we've been having, eh?"
-    ///     }
-    ///
-    /// </remarks>
-    /// <response code="204">The update was successful.</response>
-    /// <response code="400">The name is null, an empty string, or a different topic already has this name.</response>
-    /// <response code="404">No topic was found matching the ID passed into the URI path.</response>
     [HttpPut("{id:int}")]
     [Consumes("application/json")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateTopic(
-        [FromRoute] int id, 
+        [FromRoute] int id,
         [FromBody] Topic topic
         )
     {
@@ -113,7 +115,7 @@ public class TopicsController : ForumBaseController
         }
         catch (PostgresException e) when (e.SqlState == PostgresErrorCodes.UniqueViolation)
         {
-            return BadRequest("Name of topic is invalid");
+            return BadRequest("Topic name is not unique and is already defined for another topic");
         }
 
         return numberOfTopicsUpdated == 0 ? NotFound() : NoContent();
--- test/Forum.Data.Tests/Queries/TopicTests.cs
+++ test/Forum.Data.Tests/Queries/TopicTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Forum.Data.Models;
 using Forum.Data.Queries;
 using Forum.Tests.Library.Fixtures;
+using Forum.Tests.Library.Helpers;
 using Npgsql;
 using Xunit;
 
@@ -12,10 +13,12 @@ namespace Forum.Data.Tests.Queries;
 public class TopicTests : IClassFixture<DatabaseFixture>
 {
     private readonly Topics _topics;
+    private readonly TopicHelpers _helpers;
 
     public TopicTests(DatabaseFixture fixture)
     {
         _topics = new Topics(fixture.Database);
+        _helpers = new TopicHelpers(_topics);
     }
 
     [Fact]
@@ -35,7 +38,7 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     public async void GetAll_SomeTopics_ReturnsNonEmptyList()
     {
         // Arrange
-        _ = CreateMockTopic();
+        _ = _helpers.CreateMockTopic();
         
         // Act
         var topics = await _topics.GetAll();
@@ -48,7 +51,7 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     public async void GetTopicById_ValidId_ReturnsRequestedTopic()
     {
         // Arrange
-        var expected = (await CreateMockTopic()).Id;
+        var expected = (await _helpers.CreateMockTopic()).Id;
         
         // Act
         var actual = (await _topics.GetTopicById(expected))!.Id;
@@ -81,7 +84,7 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     public async void GetTopicByName_ValidName_ReturnsRequestedTopic()
     {
         // Arrange
-        var expected = (await CreateMockTopic()).Name;
+        var expected = (await _helpers.CreateMockTopic()).Name;
 
         // Act
         var actual = (await _topics.GetTopicByName(expected))!.Name;
@@ -107,6 +110,66 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     }
 
     [Theory]
+    [InlineData("Test", "")]
+    [InlineData("Testdsgsdgsd", null)]
+    [InlineData("A good name for a topic", "I agree fully")]
+    [InlineData("AH BEES", "🐝")]
+    public async void CreateTopic_ValidNameAndValidDescription_ReturnsValidIdOfNewTopic(string validName, string validDescription)
+    {
+        // Arrange
+        var newTopic = new Topic
+        {
+            Name = validName,
+            Description = validDescription
+        };
+        
+        // Act
+        var newTopicId = await _topics.CreateTopic(newTopic);
+        var foundTopic = await _topics.GetTopicById(newTopicId);
+        
+        // Assert
+        Assert.NotNull(foundTopic);
+    }
+    
+    [Theory]
+    [InlineData(null, "")]
+    [InlineData(null, null)]
+    [InlineData(null, "I agree fully")]
+    [InlineData(null, "🐝")]
+    public async void CreateTopic_NullNameAndValidDescription_ThrowsNotNullException(string invalidName, string validDescription)
+    {
+        // Arrange
+        const string expected = PostgresErrorCodes.NotNullViolation;
+        var newTopic = new Topic
+        {
+            Name = invalidName,
+            Description = validDescription
+        };
+        Func<Task<int>> createTopic = async () => await _topics.CreateTopic(newTopic);
+        
+        // Act
+        var actual = (await Record.ExceptionAsync(createTopic) as PostgresException)!.SqlState;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public async void CreateTopic_NameAlreadyExists_ThrowsUniqueException()
+    {
+        // Arrange
+        const string expected = PostgresErrorCodes.UniqueViolation;
+        var mockTopic = await _helpers.CreateMockTopic();
+        Func<Task<int>> createTopic = async () => await _topics.CreateTopic(mockTopic);
+        
+        // Act
+        var actual = (await Record.ExceptionAsync(createTopic) as PostgresException)!.SqlState;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
     [InlineData("Test")]
     [InlineData("Elbows")]
     [InlineData("fdsfds")]
@@ -117,7 +180,7 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     {
         // Arrange
         const int expected = 1;
-        var mockTopic = await CreateMockTopic();
+        var mockTopic = await _helpers.CreateMockTopic();
         mockTopic.Description = validDescription;
 
         // Act
@@ -129,34 +192,36 @@ public class TopicTests : IClassFixture<DatabaseFixture>
 
     [Theory]
     [InlineData(null)]
-    public async void UpdateTopic_InvalidName_ThrowsException(string invalidName)
+    public async void UpdateTopic_NullName_ThrowsNotNullException(string invalidName)
     {
         // Arrange
-        var mockTopic = await CreateMockTopic();
+        const string expected = PostgresErrorCodes.NotNullViolation;
+        var mockTopic = await _helpers.CreateMockTopic();
         mockTopic.Name = invalidName;
         Func<Task<int>> updateTopic = async () => await _topics.UpdateTopic(mockTopic);
         
         // Act
-        var exception = await Record.ExceptionAsync(updateTopic);
-        
+        var actual = (await Record.ExceptionAsync(updateTopic) as PostgresException)!.SqlState;
+
         // Assert
-        Assert.IsType<Npgsql.PostgresException>(exception);
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
-    public async void UpdateTopic_NameAlreadyExists_ThrowsException()
+    public async void UpdateTopic_NameAlreadyExists_ThrowsUniqueException()
     {
         // Arrange
-        var mockTopic1 = await CreateMockTopic();
-        var mockTopic2 = await CreateMockTopic();
+        const string expected = PostgresErrorCodes.UniqueViolation;
+        var mockTopic1 = await _helpers.CreateMockTopic();
+        var mockTopic2 = await _helpers.CreateMockTopic();
         mockTopic1.Name = mockTopic2.Name;
         Func<Task<int>> updateTopic = async () => await _topics.UpdateTopic(mockTopic1);
         
         // Act
-        var exception = await Record.ExceptionAsync(updateTopic);
+        var actual = (await Record.ExceptionAsync(updateTopic) as PostgresException)!.SqlState;
 
         // Assert
-        Assert.IsType<Npgsql.PostgresException>(exception);
+        Assert.Equal(expected, actual);
     }
     
     [Fact]
@@ -164,7 +229,7 @@ public class TopicTests : IClassFixture<DatabaseFixture>
     {
         // Arrange
         const int expected = 1;
-        var mockTopic = await CreateMockTopic();
+        var mockTopic = await _helpers.CreateMockTopic();
 
         // Act
         var actual = await _topics.UpdateTopic(mockTopic);
@@ -184,22 +249,5 @@ public class TopicTests : IClassFixture<DatabaseFixture>
 
         // Assert
         Assert.Equal(expected, actual);
-    }
-
-    /// <summary>
-    /// Create a mock topic in the database for testing purposes, with a randomly generated string being used for the
-    /// topic name.
-    /// </summary>
-    /// <returns>A mock topic with a randomly generated name.</returns>
-    private async Task<Topic> CreateMockTopic(string description = "Test")
-    {
-        var topic = new Topic
-        {
-            Name = Guid.NewGuid().ToString(),
-            Description = description
-        };
-        topic.Id = await _topics.CreateTopic(topic);
-
-        return topic;
     }
 }
