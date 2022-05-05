659dd15e283595b87be92aa6282908c02e938d33
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-05-05 12:30:20 -0400

    Started work on adding Blazor WASM frontend

--- src/HeavyMetalMachine.Api/Controllers/Base/HeavyMetalBaseController.cs
+++ src/HeavyMetalMachine.Api/Controllers/Base/HeavyMetalBaseController.cs
@@ -5,6 +5,4 @@ namespace HeavyMetalMachine.Api.Controllers.Base;
 [ApiController]
 [Route("api/v1/[controller]")]
 [Produces("application/json")]
-public abstract class HeavyMetalBaseController : ControllerBase
-{
-}
+public abstract class HeavyMetalBaseController : ControllerBase { }
